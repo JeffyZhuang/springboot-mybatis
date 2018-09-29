66a955f98d396c8ca404b072415ac07e6f9f2a60
JeffyZhuang <270225738@qq.com>
2018-09-29 10:33:31 +0800

    增加mybatis-generator自动生成代码组件

--- src/main/java/com/zzh/SpringbootApplication.java
+++ src/main/java/com/zzh/SpringbootApplication.java
@@ -3,9 +3,15 @@ package com.zzh;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class SpringbootApplication {
+
+    @RequestMapping("/")
+    public String main(){
+        return "Hello World!";
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApplication.class, args);
