b01555a34b726fde3caa78fcf9ba43ef29b71ddf
JeffyZhuang <270225738@qq.com>
2018-10-02 23:02:58 +0800

    增加Swagger

--- src/main/java/com/zzh/controller/UserController.java
+++ src/main/java/com/zzh/controller/UserController.java
@@ -3,6 +3,8 @@ package com.zzh.controller;
 import com.zzh.po.User;
 import com.zzh.result.ApiResult;
 import com.zzh.service.UserService;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +25,8 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @ApiOperation(value="获取用户", notes="根据userId获取用户")
+    @ApiImplicitParam(name = "id", value = "ID", required = true, dataType = "Long",paramType = "query")
     @RequestMapping(value = "/user", method = RequestMethod.GET)
     public ApiResult getUserList(@RequestParam(value = "id") int id) {
         return ApiResult.success(userService.selectByPrimaryKey(id));
--- src/main/java/com/zzh/po/User.java
+++ src/main/java/com/zzh/po/User.java
@@ -1,4 +1,5 @@
 package com.zzh.po;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 /**
@@ -8,11 +9,12 @@ import lombok.Data;
 
 @Data
 public class User {
+    @ApiModelProperty(value = "用户的id")
     private Integer userId;
-
+    @ApiModelProperty(value = "用户的名字")
     private String userName;
-
+    @ApiModelProperty(value = "用户的密码")
     private String password;
-
+    @ApiModelProperty(value = "用户的电话")
     private String phone;
 }
