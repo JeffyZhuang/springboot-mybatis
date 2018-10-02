cf690c7037ff244276822cf7794d27655a606c42
JeffyZhuang <270225738@qq.com>
2018-10-02 23:11:29 +0800

    增加spring cache

--- src/main/java/com/zzh/SpringbootApplication.java
+++ src/main/java/com/zzh/SpringbootApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @ComponentScan
+@EnableCaching
 @SpringBootApplication(scanBasePackages = "com.zzh")
 @MapperScan("com.zzh.mapper")
 public class SpringbootApplication {
--- src/main/java/com/zzh/controller/UserController.java
+++ src/main/java/com/zzh/controller/UserController.java
@@ -1,12 +1,11 @@
 package com.zzh.controller;
-
-import com.zzh.po.User;
 import com.zzh.result.ApiResult;
 import com.zzh.service.UserService;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -28,6 +27,7 @@ public class UserController {
     @ApiOperation(value="获取用户", notes="根据userId获取用户")
     @ApiImplicitParam(name = "id", value = "ID", required = true, dataType = "Long",paramType = "query")
     @RequestMapping(value = "/user", method = RequestMethod.GET)
+    @Cacheable("user")
     public ApiResult getUserList(@RequestParam(value = "id") int id) {
         return ApiResult.success(userService.selectByPrimaryKey(id));
 
