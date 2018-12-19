6c1a8fe21098238e8e8fcb98b250429290726428
JeffyZhuang <270225738@qq.com>
2018-12-19 16:25:04 +0800

    1、修改中文乱码问题 2、解决将文件放到resource目录下打包下载问题

--- src/main/java/com/zzh/controller/FileController.java
+++ src/main/java/com/zzh/controller/FileController.java
@@ -3,10 +3,7 @@ package com.zzh.controller;
 import com.zzh.result.ApiResult;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,7 +13,6 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
 
 /**
  * @Author: zzh
@@ -26,46 +22,20 @@ import java.net.URLEncoder;
 @RestController
 @Slf4j
 public class FileController {
-    @RequestMapping(value = "/upload", method = RequestMethod.GET)
-    public ApiResult startTask(@RequestParam("file") MultipartFile file) {
-        try {
-            if (file.isEmpty()) {
-                return ApiResult.fail("文件为空");
-            }
-            String fileName = file.getOriginalFilename();
-            log.info("上传的文件名为：" + fileName);
-            // 获取文件的后缀名
-            String suffixName = fileName.substring(fileName.lastIndexOf("."));
-            log.info("文件的后缀名为：" + suffixName);
-            // 设置文件存储路径
-            String filePath = "file/";
-            String path = filePath + fileName + suffixName;
-
-            File dest = new File(path);
-            // 检测是否存在目录
-            if (!dest.getParentFile().exists()) {
-                dest.getParentFile().mkdirs();// 新建文件夹
-            }
-            file.transferTo(dest);
-        } catch (IllegalStateException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return ApiResult.success("上传成功");
-    }
 
     @RequestMapping("/download")
-    public ApiResult downloadFile(HttpServletRequest request, HttpServletResponse response) throws UnsupportedEncodingException {
-        String fileName = "job_entity.sql";// 设置文件名，根据业务需要替换成要下载的文件名
+    public ApiResult downloadFile(HttpServletRequest request, HttpServletResponse response) throws
+            UnsupportedEncodingException {
+        String fileName = "输入输出DEMO.doc";
         if (fileName != null) {
             //设置文件路径
-            String realPath = "file/";
-            fileName = URLEncoder.encode(fileName, "utf-8");
-            File file = new File(realPath, fileName);
+            //String realPath = "file/";
+            response.setCharacterEncoding("utf-8");
+            File file = new File(getClass().getResource("/").getPath(), fileName);
             if (file.exists()) {
-                response.setContentType("application/force-download");// 设置强制下载不打开
-                response.addHeader("Content-Disposition", "attachment;fileName=" + fileName);// 设置文件名
+                response.setContentType("application/force-download");
+                response.addHeader("Content-Disposition", "attachment;fileName=" + new String(fileName.getBytes(),
+                        "ISO8859-1"));
                 byte[] buffer = new byte[1024];
                 FileInputStream fis = null;
                 BufferedInputStream bis = null;
